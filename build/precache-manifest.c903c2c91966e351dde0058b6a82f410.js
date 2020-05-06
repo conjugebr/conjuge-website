2bee65969e22e55dac3cdbd3c1c3ba070c273184
Santi L <idlucassanti@hotmail.com>
2020-05-06 03:07:02 -0300

    new tentantive

---
+++ build/precache-manifest.c903c2c91966e351dde0058b6a82f410.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e905f753a174b8b714f3aa50a8b3aa16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62c37959e9edca5be2d5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b90c2af06eb2cc983f36",
+    "url": "/static/js/2.8eb0afda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8eb0afda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62c37959e9edca5be2d5",
+    "url": "/static/js/main.554601d3.chunk.js"
+  },
+  {
+    "revision": "2c23097de5d0c6efdc26",
+    "url": "/static/js/runtime-main.5dfa3af0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
